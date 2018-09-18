3b499c5044c84d8830fdb9de1a162456ce26d389
leonardofelix2004 <leonardofelix2004@hotmail.com>
2018-09-18 17:03:33 -0300

    Corrigido problema no Corrs

--- BE_Izio.TransacaoRest/Global.asax.cs
+++ BE_Izio.TransacaoRest/Global.asax.cs
@@ -28,10 +28,23 @@ namespace TransacaoIzioRest
                 HttpContext.Current.Response.AddHeader("Cache-Control", "no-cache");
                 HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "*");
                 HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept, tokenAutenticacao");
-
                 HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
                 HttpContext.Current.Response.End();
             }
+
+            //if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
+            //{
+            //    HttpContext.Current.Response.AddHeader("Cache-Control", "no-cache");
+            //    HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "*");
+            //    HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "*");
+            //    HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
+            //    HttpContext.Current.Response.End();
+
+            //    HttpContext.Current.Response.Headers.Remove("Access-Control-Allow-Origin");
+            //    HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
+            //}
+
+            
         }
     }
 }
