de762eed55b1332cf1dff647e834c1365309e236
leonardofelix2004 <leonardofelix2004@hotmail>
2020-05-21 11:32:09 -0300

    Ajustado o version do Models

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TransacaoIzioRest.DAO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImportaTransacaoDAO dao = new ImportaTransacaoDAO("redeMarket");
+        }
+    }
+}
